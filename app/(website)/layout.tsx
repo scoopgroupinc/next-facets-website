547f7e4b45d3cc1337798a0763f3a63f605b6786
Natalie Pan <natalie.pan@gmail.com>
2023-09-18 23:51:34 -0700

    dating app space

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -21,7 +21,7 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: 'Facets Authentic & Mindful Dating',
   description:
-    "Find true love. We combine the latest research, psychologist experience, and people's feedback to help you build strong and deeper connections.",
+    "Find true love. We are a wellness app for singles navigating the dating space. We combine the latest research & psychology to help people build stronger and deeper connections.",
 };
 
 export default function RootLayout({
