465961b047e32da02edb582cab0c031050a43d6a
Natalie Pan <natalie.pan@gmail.com>
2023-09-19 23:24:52 -0700

    header footer on waitlist page

--- app/(website)/waitlist/page.tsx
+++ app/(website)/waitlist/page.tsx
@@ -1,15 +1,24 @@
+import Footer from '@/app/components/website/Footer';
+import Nav from '@/app/components/website/Nav';
+
 export default function Waitlist() {
-    // note sourced from https://docs.google.com/forms/d/1SeIMGNDFm3PwOb7VLHifNarrEcyOylmIMNESZQSfvII/edit
+  // note sourced from https://docs.google.com/forms/d/1SeIMGNDFm3PwOb7VLHifNarrEcyOylmIMNESZQSfvII/edit
   return (
-    <iframe
-      src="https://docs.google.com/forms/d/e/1FAIpQLSfCtfqzQsW1soTfnoNKHToXbNFAbCjI3v4FVP-Qf-80ppyfbA/viewform?embedded=true"
-      width="640"
-      height="2984"
-      frameBorder="0"
-      marginHeight={0}
-      marginWidth={0}
-    >
-      Loading…
-    </iframe>
+    <>
+      <Nav />
+      <div style={{ paddingTop: 100 }}>
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSfCtfqzQsW1soTfnoNKHToXbNFAbCjI3v4FVP-Qf-80ppyfbA/viewform?embedded=true"
+          width="640"
+          height="2984"
+          frameBorder="0"
+          marginHeight={0}
+          marginWidth={0}
+        >
+          Loading…
+        </iframe>
+      </div>
+      <Footer />
+    </>
   );
 }
