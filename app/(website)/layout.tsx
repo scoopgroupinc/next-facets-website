a6a7a1a8386d7a8dd0f04e558e01753ee4ccc9b3
Natalie Pan <natalie.pan@gmail.com>
2023-09-25 13:52:55 -0700

    <meta name="google-site-verification" content="vMd6_VKR6nVlLEhqn7u45kEZpG9HSAE5PEtuzdBQ8Mg" />

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={poppins.className}>{children}</body>
       <Script
         id="adsbygoogle-init"
         strategy="afterInteractive"
